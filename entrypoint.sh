3b1e0a97b866b771a0f071643cb2d1f77c7e51a3
ebinikoo <126134386+ebinikoo@users.noreply.github.com>
2023-04-17 13:55:52 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
+UUID='https:/2B335F8D-FBD9-4FD4-BF46-8750E147F8CB/'
 VMESS_WSPATH='/vmess'
 VLESS_WSPATH='/vless'
 TROJAN_WSPATH='/trojan'
